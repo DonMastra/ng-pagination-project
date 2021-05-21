6e292cf4ab8991493c78b058d7ca390fd6501f64
Andres Mastracchio <mastracchioandres@gmail.com>
2021-05-21 07:56:18 -0300

    initial commit

---
+++ src/app/pokemon/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
+import { Pokemon } from '../interfaces/pokemon.interfaces';
+
+
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.css']
+})
+export class PokemonListComponent implements OnInit {
+
+  public pokemons: Pokemon[] = [];
+  page: number = 0;
+  search: string = '';
+
+  constructor( private pokemonService: PokemonService) { }
+
+  ngOnInit(): void {
+    this.pokemonService.getAllPokemons().subscribe(
+      pokemons => {
+        this.pokemons = pokemons;
+      }
+    )
+  }
+
+  nextPage() {
+    this.page += 5;
+  }
+
+  previousPage() {
+    if( this.page > 0) {
+      this.page -= 5;
+    }
+  }
+
+  onSearchPokemon( search: string ){
+    this.page = 0;
+    this.search = search;
+  }
+
+}
